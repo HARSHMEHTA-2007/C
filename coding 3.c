e7603cc66a8e4e37ac1d6471bec16257496fa913
HARSHMEHTA-2007 <harshmehta5916@gmail.com>
2025-09-08 15:06:04 +0530

    Add files via upload

---
+++ coding 3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],i;
+    printf("enter ten values:");
+    for(i=0;i<=9;i++)
+scanf("%d",&a[i]);
+
+    for(i=3;i<=3;i++)
+    printf(" 4th value is %d\n",a[i]);
+
+    for(i=8;i<=8;i++)
+    printf(" 9th value is %d\n",a[i]);
+
+    for(i=6;i<=6;i++)
+    printf(" 7th value is %d\n",a[i]);
+
+}
+
